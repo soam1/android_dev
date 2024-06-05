f6e2eb897bf71a5e6e3381d8c5ac709f71cddd42
soam1 <soamakash9@gmail.com>
2024-06-05 16:29:11 +0900

    setting intent for the register here button

--- app/src/main/java/com/example/housemartapplication/login/LoginActivity.kt
+++ app/src/main/java/com/example/housemartapplication/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.housemartapplication.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.housemartapplication.R
+import com.example.housemartapplication.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     lateinit var passwordLoginEdtText: EditText
@@ -32,7 +34,7 @@ class LoginActivity : AppCompatActivity() {
         passwordLoginEdtText = findViewById(R.id.password_login_et)
 
         findViewById<Button>(R.id.signup_link_btn).setOnClickListener {
-            //startActivity(Intent(this, SignUpActivity::class.java))
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
 
         findViewById<Button>(R.id.login_btn).setOnClickListener {
